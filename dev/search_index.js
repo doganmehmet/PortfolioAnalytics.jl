96016c16fdca757523730414e521184ec3401236
Documenter.jl <documenter@juliadocs.github.io>
2023-02-17 08:15:21 +0000

    build based on a2dfdbd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#PortfolioAnalytics.jl","page":"PortfolioAnalytics.jl","title":"PortfolioAnalytics.jl","text":"","category":"section"},{"location":"","page":"PortfolioAnalytics.jl","title":"PortfolioAnalytics.jl","text":"Documentation for PortfolioAnalytics.jl","category":"page"},{"location":"","page":"PortfolioAnalytics.jl","title":"PortfolioAnalytics.jl","text":"CurrentModule = PortfolioAnalytics","category":"page"},{"location":"","page":"PortfolioAnalytics.jl","title":"PortfolioAnalytics.jl","text":"PortfolioAnalytics.Return()","category":"page"}]
+}
